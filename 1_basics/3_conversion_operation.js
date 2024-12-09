67edccff7ebd5f641b02799557b132aeed000be5
pajaz07 <149667733+pajaz07@users.noreply.github.com>
2024-12-09 06:34:53 +0000

    conversion_4

--- 1_basics/3_conversion_operation.js
+++ 1_basics/3_conversion_operation.js
@@ -22,3 +22,32 @@ console.log(1 + 2 + "2"); // 32
 
 // == check if values are equal ("2" == 2 => true)
 // === strict check: also checks datatype ("2" === 2 => false)
+
+/*
+Primitive datatypes:
+    1. String
+    2. Number
+    3. Boolean
+    4. null
+    5. undefined
+    6. Symbol
+    7. BigInt
+
+Non-primitive (reference):
+    1. Array
+    2. Objects
+    3. Functions
+
+*/
+
+const arr_letters = ["aaa", "bbb", "ccc"]
+
+let myObj={
+    name: "Parth",
+    age: 20,
+}
+
+const myFunc = function(){
+    console.log("hellooo");
+    
+}
