f0d7b964f30b00f953f773610a77007228ca49ef
pajaz07 <149667733+pajaz07@users.noreply.github.com>
2024-12-09 06:20:20 +0000

    conversion_2

--- 1_basics/3_conversion_operation.js
+++ 1_basics/3_conversion_operation.js
@@ -10,3 +10,15 @@ it should not be converted to number
 but it will be converted and the value of scoreinNum will be NaN (not a number)
 It is a small issue in JS
 */
+
+let val=3
+let negval = -val //negation
+
+// These are not preferrable 
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+// == check if values are equal ("2" == 2 => true)
+// === strict check: also checks datatype ("2" === 2 => false)
